a188d59f1cded0853f910cb991f12ab4c0e562c1
Wendellmeset <69725697+wendellmeset@users.noreply.github.com>
2021-06-17 09:51:21 -0700

    Added source via upload

---
+++ source/MultiPurpose-Window/MainForm.Designer.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: matti
+ * Date: 6/16/2021
+ * Time: 3:43 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace MultiPurpose_Window
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(922, 513);
+			this.Name = "MainForm";
+			this.Text = "MultiPurpose-Window";
+			this.ResumeLayout(false);
+		}
+	}
+}
