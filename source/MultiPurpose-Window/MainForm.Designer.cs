65af5947258f552f453395e75ee67d474c4274ba
Wendellmeset <69725697+wendellmeset@users.noreply.github.com>
2021-07-03 15:39:07 -0700

    Update MainForm.Designer.cs

--- source/MultiPurpose-Window/MainForm.Designer.cs
+++ source/MultiPurpose-Window/MainForm.Designer.cs
@@ -1,11 +1,3 @@
-﻿/*
- * Created by SharpDevelop.
- * User: matti
- * Date: 6/16/2021
- * Time: 3:43 PM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
 namespace MultiPurpose_Window
 {
 	partial class MainForm
