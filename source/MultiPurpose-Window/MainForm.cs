477f6cf712dbdb921d211a60e25ec9f38f2c002d
Wendellmeset <69725697+wendellmeset@users.noreply.github.com>
2021-07-03 15:38:51 -0700

    Update MainForm.cs

--- source/MultiPurpose-Window/MainForm.cs
+++ source/MultiPurpose-Window/MainForm.cs
@@ -1,11 +1,3 @@
-﻿/*
- * Created by SharpDevelop.
- * User: matti
- * Date: 6/16/2021
- * Time: 3:43 PM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
 using System;
 using System.Collections.Generic;
 using System.Drawing;
